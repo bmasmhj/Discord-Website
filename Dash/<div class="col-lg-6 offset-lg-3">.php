d590ae00d908cf2c60d84d869aff22e16c05e98a
Bimash Maharjan <bleachmhj@gmail.com>
2022-06-09 14:23:53 +0545

    some  minor change

--- Dash/<div class="col-lg-6 offset-lg-3">.php
+++
@@ -1,8 +0,0 @@
-<div class="col-lg-6 offset-lg-3">
-                    <div class="head text-white">
-                    <h3 id="pagename">Bimash Maharjan</h3>
-                    <span>Followers (<span id="folow">0</span>)</span>
-                    </div>
-                    <div id="post">
-
-                    </div>
